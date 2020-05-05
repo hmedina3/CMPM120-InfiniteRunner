cbb94989851590af803cca42fe2bc02d163558a5
Yongshi <yosun@ucsc.edu>
2020-05-04 23:44:27 -0700

    collectables done

--- prefabs/Betty.js
+++ prefabs/Betty.js
@@ -8,60 +8,33 @@ class Betty extends Phaser.GameObjects.Sprite {
         
         scene.add.existing(this); // add object to existing scene
         this.isJumping = false; // tracks players jumping status
+
         // adds jump audio
-        this.sfxJump = scene.sound.add('sfx_jump');
+        this.sfxJump = scene.sound.add('sfx_jumpup');
     }
 
     update(){
 
-        // Jump button
-      if(Phaser.Input.Keyboard.JustDown(keySPACE)){
-        this.isJumping = true;
-        // play sfx
-        this.sfxJump.play();
+      // left/right movement
+      if(keyLEFT.isDown && this.x >= 0){
+        //speed depends on this
+        this.x -= 2;
+      }else if (keyRIGHT.isDown && this.x <= 600){
+        this.x += 2;
       }
-        // if player jumps, move up
-        if(this.isJumping && this.y >= 108){
-            // move player forward
-           this.y -= 2;
-    
-          } // end of if jumping player.
-          
 
-        // jump button (spacebar)
-        // isDown =  if it was pressed this frame, do this
-        // JustDown = if it was pressed last frame, do this; this is
-        //      to prevent player from holding down F to fire, they 
-        //      must manually do it to fire
-    
-    // control player while jumping
-      if(this.isJumping){
-        if(keyLEFT.isDown && this.x >= 47){
-          this.x -= 2;
-        }
-        else if(keyRIGHT.isDown && this.x <= 578){
-          this.x += 2;
-        }
-        else{
-        if (Phaser.Input.Keyboard.JustDown(keySPACE) && !this.isJumping){
-            this.isJumping = true;
-            this.jump();
-            this.sfxJump.play(); // play sfx
-        }
+      //jumping
+      if (Phaser.Input.Keyboard.JustDown(keySPACE) && !this.isJumping){
+        this.isJumping = true;
+        this.jump();
+        this.sfxJump.play(); // play sfx
+      }
 
         //only allow jump if betty is touching ground (to prevent double jump)
         if(this.body.velocity.y ==0){
             this.isJumping = false;
         }
-      }
-
-    }
-  }
-
-    // reset player to ground
-    reset(){
-        this.isJumping = false;
-        this.y = 350; //431
+      
     }
     
     jump(){
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,40 +10,63 @@ class Play extends Phaser.Scene {
 
         // load audio
         this.load.audio('sfx_power', './assets/powerup.wav'); //https://freesound.org/people/evan.schad/sounds/470768/
-        this.load.audio('sfx_coin', './assets/collectcoin.wav'); //https://freesound.org/people/bradwesson/sounds/135936/ 
-        this.load.audio('sfx_jump', './assets/small-jump-for-videogames.wav'); //https://freesound.org/people/simoneyoh3998/sounds/500675/
-        this.load.audio('sfx_stab', './assets/stab.wav'); //https://freesound.org/people/InspectorJ/sounds/413496/
-        this.load.audio('sfx_throw', './assets/throw.wav'); //https://freesound.org/people/kylepyke/sounds/196562/
-       
+        this.load.audio('sfx_coin', './assets/collectcoin.wav'); //https://freesound.org/people/bradwesson/sounds/135936/
+        this.load.audio('sfx_jumpup', './assets/small-jump-for-videogames.wav'); //https://freesound.org/people/simoneyoh3998/sounds/500675/
+        
         // load images/tile sprites
         this.load.image('betty','./assets/betty1.png'); //https://opengameart.org/content/one-more-lpc-alternate-character
         this.load.image('ground','./assets/RockTileSet2020.png'); // https://opengameart.org/content/stone-ground
-            
+        this.load.image('shoe','./assets/shoe.png');
+        
+        //icons
+        this.load.image('clock','./assets/Monsters_2/AlarmClock/AlarmClock_32x32_blue_damage_R.png'); //https://opengameart.org/content/cute-sprites-pack-1 
+        this.load.image('score_icon','./assets/star_coin_animation_Pack/star_coin_shine/star_coin_1.png');
+    
         // background picture
         this.load.image('background','./assets/cavernous.png'); //https://opengameart.org/content/cavernous-background
         
         // load spritesheet
-        this.load.spritesheet('coin','./assets/spin_coin_big_strip6.png',{frameWidth: 192, frameheight: 191, startFrame: 0, endFrame: 20});
+        this.load.spritesheet('coin','./assets/spin_coin_big_upscale_strip6.png',{frameWidth: 18, frameheight: 20, startFrame: 0, endFrame: 20});
+        
+        
     }
 
     create() {
 
-      // This will make the background move as a parallax scroller. - H.
-      this.bg_1 = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'background');
-      this.bg_1.setScale(1.8);
-      this.bg_1.setOrigin(0, 0);
-      this.bg_1.setScrollFactor(0);
+        // This will make the background move as a parallax scroller. - H.
+        this.bg_1 = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'background');
+        this.bg_1.setScale(1.8);
+        this.bg_1.setOrigin(0, 0);
+        this.bg_1.setScrollFactor(0);
 
-     // play music
-      music = this.sound.add('sfx_music');
-      music.play();
+        //adding sound effects
+        this.sfxCoin = this.sound.add('sfx_coin');
+        this.sfxShoe = this.sound.add('sfx_power');
 
-    // place tile sprite
+        // play bg music
+        this.bgm = this.sound.add('sfx_music');
+        this.bgm.loop = true;
+        this.bgm.play()
+
+         // place tile sprite
         this.ground = this.add.tileSprite(0, 0, game.config.width, 48, 'ground');
         this.ground.setOrigin(0, 0);
         this.ground.setScrollFactor(0);
-    // since this tile is shorter I positioned it at the bottom of the screen
+        this.physics.add.existing(this.ground);
+
+        //score and timer icon
+        this.clock = this.add.image(380,20,'clock');
+        this.score_icon = this.add.image(33,25,'score_icon').setScale(.100,.100)
+
+
+        // since this tile is shorter I positioned it at the bottom of the screen
         this.ground.y = 12 * 32.5;
+        
+        //coins and powerups
+        this.coin = this.add.sprite(200,300,'coin'); //adding coins at x and y axis
+        this.physics.add.existing(this.coin);
+        this.shoe = this.add.sprite(300,300,'shoe');
+        this.physics.add.existing(this.shoe);
 
         
         // UI to keep track of points
@@ -58,41 +81,37 @@ class Play extends Phaser.Scene {
         // borders?
         
         // main character postion
-        this.p1Betty = new Betty(this, game.config.width/2,350,'betty').setScale(1.5,1.5).setOrigin(0,0);
+        this.p1Betty = new Betty(this, game.config.width/2,300,'betty').setScale(1.5,1.5).setOrigin(0,0);
+        this.physics.add.existing(this.p1Betty); //adding physics to betty
+        this.p1Betty.body.setSize(30,32,0,0); //setting collision box size
+        this.p1Betty.body.gravity.y = 100; //adding gravity
+
+        // define movement and colliders
+        this.physics.add.collider(this.p1Betty,this.ground1);
+        this.physics.add.collider(this.p1Betty,this.ground2);
+        this.physics.add.collider(this.p1Betty,this.ground);
        
         // define keyboard keys for movement
-       keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-       keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-       keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-       keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R); 
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R); 
         
         // animation configs
         this.anims.create({
-          key: 'explode',
-          frames: this.anims.generateFrameNumbers('explosion',{start:0, end: 9, first: 0}),
-          framerate: 30,
+            key: 'rotate',
+            frames: this.anims.generateFrameNumbers('coin',{start:0, end: 5, first: 0}),
+            framerate: 30,
+            repeat: -1,
         })
+        this.coin.anims.play('rotate', true);
+        
 
         // UI to keep track of points
+
         // score
         this.p1Score = 0
 
-        // score display
-        let scoreConfig = {
-          fontFamily: 'Helvetica',
-          fontSize: '28px',
-          backgroundColor: '#Ff4343',
-          color: '#F3B141',
-          align: 'right',
-          padding: {
-              top: 3,
-              bottom: 3,
-          },
-          fixedWidth: 60
-        }
-        
-        // score position
-        this.scoreLeft = this.add.text(5, 5, this.p1Score, scoreConfig)
         
         // Time UI
     
@@ -101,22 +120,43 @@ class Play extends Phaser.Scene {
     }   // end of create
 
     update() {
+
+        //overlap detection with powerups and coins
+        this.physics.add.overlap(this.p1Betty,this.coin,this.pickCoin,null,this);
+        this.physics.add.overlap(this.p1Betty,this.shoe,this.pickShoe,null,this); 
+
+        //making ground immovable
+        this.ground.body.immovable = true;
+
         // scrolls the background
          this.bg_1.tilePositionX += 0.3;
 
-        // timer going down each second
-        game.settings.gameTimer = game.settings.gameTimer - 1000;
+        //show score
+        //this.scoreLeft = this.add.text(55, 5, this.p1Score, scoreConfig);
+        this.scoreLeft = this.add.text(55, 5, this.p1Score, scoreConfig);
 
-        // move to death scene once timer runs out or if betty runs into death pits
+        //show timer
+        if (this.gameOver == false){
+            this.timerRight = this.add.text(400,5,game.settings.gameTimer/1000,timerConfig);
+        }else{
+            this.timerRight = this.add.text(400,5,0.000,timerConfig); 
+        }
+
+        //timer going down each second
+        game.settings.gameTimer = game.settings.gameTimer - 17;
+
+        //move to death scene once timer runs out or if betty runs into death pits
         if(game.settings.gameTimer <= 0 || this.p1Betty.y > game.config.height){
             this.gameOver = true;
-         // move to death scene once timer runs out
-            music.stop();
-            this.add.text(game.config.width/2, game.config.height/2 + 100, "Current Highscore: "+localStorage.getItem("highscore"),highScoreConfig).setOrigin(0.5);
-            this.add.text(game.config.width/2, game.config.height/2 + 64, '<- to Restart or -> for Menu', scoreConfig).setOrigin(0.5);
+            this.bgm.stop();
+            this.add.text(game.config.width/2, game.config.height/4 + 50, 'Current Highscore: ' + localStorage.getItem("highscore"),highScoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 50, '<- to Restart or -> for Menu', deathConfig).setOrigin(0.5);
 
             // check for input during death scene
             if(Phaser.Input.Keyboard.JustDown(keyLEFT)){
+                this.scene.restart(this.p1Score);
+                game.settings.gameTimer = 15000;
+                this.bgm.destroy();
                 this.scene.start('playScene');
             }
             if(Phaser.Input.Keyboard.JustDown(keyRIGHT)){
@@ -149,26 +189,63 @@ class Play extends Phaser.Scene {
 
         //extended class update
         this.p1Betty.update(); // runs update function in Betty.js
+        this.ground.update();
         this.ground1.update();
         this.ground2.update();
     
         // check collisions
-        
+        /*
+        this.collected = false;
+        if(this.checkCollision(this.p1Betty, this.coin)){
+            this.collected = true;
+        }
+
+        if(this.collected == true){
+            this.collected = false;
+            this.p1Score += 1;
+            game.settings.gameTimer += 500;
+            this.coin.destroy();
+        }
+        */
     }
       
     formatTime(milliseconds){
         return milliseconds / 1000;
     }
 
+    /*
     onEvent(){
         if(this.timer > 0){
             this.timer -= 1;
         }
         this.Right.setText(this.timer);
-    }
+    }*/
       
-    checkCollision() {
-        
+    checkCollision(rocket, ship){
+        //simple AABB checking
+        if (rocket.x < ship.x + ship.width &&
+            rocket.x + rocket.width > ship.x &&
+            rocket.y < ship.y + ship.height &&
+            rocket.height + rocket.y + ship.y){
+                return true;
+        }else{
+            return false;
+        }
+    }
+
+    pickCoin(){;
+        this.p1Score += 1;
+        game.settings.gameTimer += 500;
+        this.sfxCoin.play();
+        this.coin.destroy();
+    }
+
+    pickShoe(){;
+        //this.countdown = this.time.delayedCall(100, () =>{
+        //},null,this);
+        this.p1Score += 5; 
+        this.sfxShoe.play();
+        this.shoe.destroy();
     }
    
 
